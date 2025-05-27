44ec0df683a2e1ae6c341522f9d45975a7089fc3
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-05-27 21:57:43 +0000

    answer php done

--- answer.php
+++ answer.php
@@ -4,33 +4,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Multiplication with while loops, in PHP" />
+  <meta name="description" content="product of even and odd numbers using loops, in PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Trisha Ray" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link
-    rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
-  <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href="./apple-touch-icon.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="./favicon-32x32.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="16x16"
-    href="./favicon-16x16.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>Multiplication with while loops, in PHP</title>
+  <title>product of even and odd numbers using loops, in PHP</title>
 </head>
 
 <body>
@@ -38,13 +23,15 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Multiplication with while loops, in PHP</span>
+        <span class="mdl-layout-title">product of even and odd numbers using loops, in PHP</span>
       </div>
     </header>
+
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/loop.png" alt="loop" />
+        <img src="./images/multiplication.jpeg" alt="multiplication" />
       </div>
+
       <div class="page-content-php">
         <div id="user-info">
           <?php
@@ -57,53 +44,59 @@
             $secondNumber = $temp;
           }
 
-          $tempFirst = $firstNumber;
-          while ($tempFirst >= 2) {
-            $tempFirst = $tempFirst - 2;
+          $checkFirst = $firstNumber;
+          while ($checkFirst >= 2) {
+            $checkFirst = $checkFirst - 2;
           }
-          $firstIsEven = ($tempFirst == 0);
+          $firstIsEven = ($checkFirst == 0);
 
-          $tempSecond = $secondNumber;
-          while ($tempSecond >= 2) {
-            $tempSecond = $tempSecond - 2;
+          $checkSecond = $secondNumber;
+          while ($checkSecond >= 2) {
+            $checkSecond = $checkSecond - 2;
           }
-          $secondIsEven = ($tempSecond == 0);
+          $secondIsEven = ($checkSecond == 0);
 
           if ($firstIsEven && $secondIsEven) {
             $product = 1;
-            $i = $firstNumber;
-            while ($i <= $secondNumber) {
-              $tempI = $i;
-              while ($tempI >= 2) {
-                $tempI = $tempI - 2;
+            $currentNumber = $firstNumber;
+
+            while ($currentNumber <= $secondNumber) {
+              $checkCurrent = $currentNumber;
+              while ($checkCurrent >= 2) {
+                $checkCurrent = $checkCurrent - 2;
               }
-              if ($tempI == 0) {
-                $product = $product * $i;
+              if ($checkCurrent == 0) {
+                $product = $product * $currentNumber;
               }
-              $i++;
+              $currentNumber++;
             }
-            echo "Even numbers between $firstNumber and $secondNumber multiplied give: $product";
+
+            echo "<h4>Even numbers between $firstNumber and $secondNumber multiplied give: $product</h4>";
           } else if (!$firstIsEven && !$secondIsEven) {
             $product = 1;
-            $i = $firstNumber;
-            while ($i <= $secondNumber) {
-              $tempI = $i;
-              while ($tempI >= 2) {
-                $tempI = $tempI - 2;
+            $currentNumber = $firstNumber;
+
+            while ($currentNumber <= $secondNumber) {
+              $checkCurrent = $currentNumber;
+              while ($checkCurrent >= 2) {
+                $checkCurrent = $checkCurrent - 2;
               }
-              if ($tempI != 0) {
-                $product = $product * $i;
+              if ($checkCurrent != 0) {
+                $product = $product * $currentNumber;
               }
-              $i++;
+              $currentNumber++;
             }
-            echo "Odd numbers between $firstNumber and $secondNumber multiplied give: $product";
+
+            echo "<h4>Odd numbers between $firstNumber and $secondNumber multiplied give: $product</h4>";
           } else {
-            echo "Please enter two even numbers or two odd numbers.";
+            echo "<h4>Please enter two even numbers or two odd numbers.</h4>";
           }
           ?>
         </div>
+
         <div class="page-content-answer">
-          <a href="./index.php">Return...</a>
+          <br>
+          <a class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" href="./index.php">Go Back</a>
         </div>
       </div>
     </main>
